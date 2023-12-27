3a0aa8638cd5608a68ea9f7656301ba2a6f5bfa2
tonytran88 <86514005+tonytran88@users.noreply.github.com>
2023-12-27 20:08:57 +0700

    Update index.php
    Signed-off-by: tonytran88 <86514005+tonytran88@users.noreply.github.com>

--- index.php
+++ index.php
@@ -16,10 +16,6 @@ if (isset($_SESSION['usuario'])){
     <title>Página de Inicio</title>
     <!-- Agregar los estilos de Bootstrap -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Caveat:wght@400;500;600&family=Dancing+Script:wght@400;500;600;700&family=Eczar:wght@500&family=Kaushan+Script&family=Kumbh+Sans:wght@400;700&family=Lobster&family=M+PLUS+1p:wght@400;500;700;800&family=Noto+Sans:ital,wght@1,200;1,300;1,400;1,500&family=Pacifico&family=Permanent+Marker&family=Poppins:wght@200;400;500;600&family=Raleway:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,500;0,700;0,900;1,400;1,500;1,700;1,900&family=Rubik:wght@300&family=Satisfy&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
